cf0c3e5f4a07e4a4f9f8d84951150b503344cf18
Oleksandr Yushchenko <sanyeks@gmail.com>
2023-01-31 17:58:12 +0200

    Add new files

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from "lodash.throttle";
 
+const STORAGE_KEY = "feedback-form-state";
+
+const form = document.querySelector(".feedback-form");
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
+
+form.addEventListener("submit", onFormSubmit);
+form.addEventListener("input", throttle(onFormInput, 500));
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  console.log(localStorage.getItem(STORAGE_KEY));
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormInput() {
+  const formData = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+localStorageSaveData();
+
+function localStorageSaveData() {
+  let savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedData) {
+    email.value = savedData.email;
+    message.value = savedData.message;
+  }
+}
